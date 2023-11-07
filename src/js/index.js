b0b62cf116b01673abec35882afdfeb5b8850323
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-11-07 22:20:19 +0300

    верстка

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.btn-change');
+const form = document.querySelector('.container-input');
+
+btn.addEventListener('submit', function (e) {
+   e.preventDefault();
+})
+form.addEventListener('submit', function (e) {
+   e.preventDefault();
+})
